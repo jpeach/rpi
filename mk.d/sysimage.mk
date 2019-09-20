5a5b662d316ce8d76b6249df560017bf900b6660
James Peach <jpeach@apache.org>
2019-09-20 00:00:11 -0700

    Add buildroot image building targets.

--- mk.d/sysimage.mk
+++ mk.d/sysimage.mk
@@ -1,10 +1,16 @@
 Sysimage_Defconfig := board/rpi4/defconfig
 
+# Build an install image.
 sysimage: build/sysimage/.config
+	$(MAKE) \
+		BR2_DL_DIR=$$(pwd)/archives \
+		O=$$(pwd)/build/sysimage \
+		-C build/buildroot
 
+# Set our pre-generated config as the default.
 build/sysimage/.config: board/rpi4/defconfig
 	$(MAKE) \
-		BR2_DL_DIR=archives \
+		BR2_DL_DIR=$$(pwd)/archives \
 		BR2_DEFCONFIG=$$(pwd)/$(Sysimage_Defconfig) \
 		O=$$(pwd)/build/sysimage \
 		-C build/buildroot \
